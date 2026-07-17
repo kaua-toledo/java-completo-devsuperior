3ba5882594bdf128976688096e9ca5d251bfcb5c
Kauã Toledo <toledokaua05@gmail.com>
2026-07-17 17:57:07 -0300

    Initial commit

---
+++ src/application/ProgramTriangle.java
@@ -0,0 +1,48 @@
+package application;
+
+import entities.Triangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramTriangle {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.println("Enter the measures of triangle X:");
+        System.out.print("A: ");
+        x.a = sc.nextDouble();
+        System.out.print("B: ");
+        x.b = sc.nextDouble();
+        System.out.print("C: ");
+        x.c = sc.nextDouble();
+        System.out.println("Enter the measures of triangle Y:");
+        System.out.print("A: ");
+        y.a = sc.nextDouble();
+        System.out.print("B: ");
+        y.b = sc.nextDouble();
+        System.out.print("C: ");
+        y.c = sc.nextDouble();
+
+        double areaX = x.area();
+        double areaY = y.area();
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Larger area: X");
+        } else {
+            System.out.println("Larger area: Y");
+        }
+
+        sc.close();
+    }
+}
---
+++ src/entities/Product.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Product {
+
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    public int addProducts(int quantity) {
+        return this.quantity += quantity;
+    }
+
+    public int removeProducts(int quantity) {
+        return this.quantity -= quantity;
+    }
+}
+
