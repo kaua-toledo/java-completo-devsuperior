6135a8ae35a69b89ca6084af3e89619d7bbe49b4
Kauã Toledo <toledokaua05@gmail.com>
2026-09-09 18:23:25 -0300

    feat: implement tax payer calculation with inheritance and polymorphism
    Create an abstract TaxPayer class with common name and annual income attributes and an abstract tax() method.
    
    Implement Individual and Company subclasses with specific tax calculation rules. Individual taxpayers calculate taxes based on annual income with a deduction for 50% of health expenditures, while Company taxpayers use different tax rates depending on the number of employees.
    
    Use polymorphism to store different taxpayer types in a List<TaxPayer> and calculate individual and total taxes through the overridden tax() method.
    
    Apply encapsulation with private attributes, constructors, getters, setters, inheritance, method overriding, and abstraction.

---
+++ src/exercises/Program.java
@@ -0,0 +1,71 @@
+package exercises;
+
+import exercises.entities.Company;
+import exercises.entities.Individual;
+import exercises.entities.TaxPayer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of tax payers: ");
+        int N = sc.nextInt();
+
+        String name;
+        Double annualIncome;
+        Double healthExpenditures;
+        Integer numberOfEmployees;
+
+        List<TaxPayer> list = new ArrayList<TaxPayer>();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println("\nTax Payer #" + (i + 1) + " data:");
+            System.out.print("Individual or Company? (I/C): ");
+            char typeOfTaxPayer = sc.next().toLowerCase().charAt(0);
+            sc.nextLine();
+            switch (typeOfTaxPayer) {
+                case 'i':
+                    System.out.print("Name: ");
+                    name = sc.nextLine();
+                    System.out.print("Annual income: ");
+                    annualIncome = sc.nextDouble();
+                    System.out.print("Health expenditures: ");
+                    healthExpenditures = sc.nextDouble();
+                    list.add(new Individual(name, annualIncome, healthExpenditures));
+                    break;
+                case 'c':
+                    System.out.print("Name: ");
+                    name = sc.nextLine();
+                    System.out.print("Annual income: ");
+                    annualIncome = sc.nextDouble();
+                    System.out.print("Number of Employees: ");
+                    numberOfEmployees = sc.nextInt();
+                    list.add(new Company(name, annualIncome, numberOfEmployees));
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }
+
+        System.out.println("\nTAXES PAID:");
+        for (TaxPayer tp : list) {
+            System.out.printf("%s: $%.2f \n", tp.getName(), tp.tax());
+        }
+
+        double sumTaxes = 0;
+        for (TaxPayer tp1 : list) {
+            sumTaxes += tp1.tax();
+        }
+        System.out.printf("\nTOTAL TAXES: $%.2f\n", sumTaxes);
+
+        sc.close();
+    }
+}
---
+++ src/exercises/entities/Individual.java
@@ -0,0 +1,35 @@
+package exercises.entities;
+
+public class Individual extends TaxPayer {
+
+    private Double healthExpenditures;
+
+    public Individual() {
+    }
+
+    public Individual(String name, double annualIncome, Double healthExpenditures) {
+        super(name, annualIncome);
+        this.healthExpenditures = healthExpenditures;
+    }
+
+    public Double getHealthExpenditures() {
+        return healthExpenditures;
+    }
+
+    public void setHealthExpenditures(Double healthExpenditures) {
+        this.healthExpenditures = healthExpenditures;
+    }
+
+    @Override
+    public double tax() {
+        double basicTax = (getAnnualIncome() < 20000.00) ? getAnnualIncome() * 0.15 : getAnnualIncome() * 0.25;
+
+        basicTax -= getHealthExpenditures() * 0.5;
+        if (basicTax < 0) {
+            basicTax = 0.0;
+        }
+
+        return basicTax;
+    }
+
+}
